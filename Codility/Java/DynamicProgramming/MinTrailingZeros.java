e70c010d3adbdc0ec7af37cfa861854bd1405cef
Suyash906 <suyash90682711@gmail.com>
2019-09-05 23:26:22 -0700

    Krypton challenge 44% test cases passed

--- Codility/Java/DynamicProgramming/MinTrailingZeros.java
+++ Codility/Java/DynamicProgramming/MinTrailingZeros.java
@@ -10,37 +10,38 @@ class MinTrailingZeros {
         int i,j, a, b, min;
         int n = A.length;
         int[][] T = new int[n][n];
-        int[][] P = new int[n][n];
+        long[][] P = new long[n][n];
+        int[][] TZ = new int[n][n];
         for(i=0;i<n;i++){
             for(j=0;j<n;j++){
                 min = Integer.MAX_VALUE;
                 if(i == 0 && j ==0 ){
-                    min = trailZeros(A[i][j]);     
+                    min = TZ[0][0] = trailZeros(A[i][j]);     
                 }
                 T[i][j] = trailZeros(A[i][j]);
-                P[i][j] = A[i][j];
+                P[i][j] = (long)A[i][j];
                 if((i-1)>=0){
-                    a = trailZeros(A[i-1][j] * A[i][j]);
+                    a = TZ[i-1][j] + trailZeros(A[i][j]);
                     if(a<min) {
                         min = a;
-                        P[i][j] = A[i-1][j] * A[i][j];
+                        P[i][j] = (long)P[i-1][j] * (long)A[i][j];
                     }
                 }
                 if((j-1)>=0){
-                    b = trailZeros(A[i][j-1] * A[i][j]);
+                    b = TZ[i][j-1] + trailZeros(A[i][j]);
                     if(b<min){
                         min = b;
-                        P[i][j] = A[i][j-1] * A[i][j];
+                        P[i][j] = (long)P[i][j-1] * (long)A[i][j];
                     }
                 }
-                T[i][j] = min;
-                A[i][j] = P[i][j];
+                TZ[i][j] = T[i][j] = min;
+                //P[i][j];
             }
         }
         return P[n-1][n-1] == 0 ? 1 :T[n-1][n-1];
     }
     
-    static int trailZeros(int x){
+    static int trailZeros(long x){
         int count=0;
         while(x != 0){
             if(0 == x || x%10 != 0)
