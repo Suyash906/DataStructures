6617a28a94dbbac1fa106e2ec8bb29ec70606c5a
Suyash906 <suyash@indusintel.com>
2020-04-13 02:01:40 -0700

    comment added for leetcode 525

--- Python3/LeetCode/525_Contiguous_Array.py
+++ Python3/LeetCode/525_Contiguous_Array.py
@@ -1,5 +1,7 @@
 class Solution:
     def findMaxLength(self, nums: List[int]) -> int:
+        # Track all the unique values of count in yaxis_map with the least index
+        # If the count value is same at multipe index, it indicates that the number of 0 and 1 are same
         maxlen = 0
         yaxis_map = {}
         yaxis_map[0] = -1
