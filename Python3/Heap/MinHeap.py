7f20df874247376a1df5a90b86eecea796b91492
Suyash Srivastava <suyash90682711@gmail.com>
2020-11-10 11:49:53 -0800

    min heap code update

--- Python3/Heap/MinHeap.py
+++ Python3/Heap/MinHeap.py
@@ -1,77 +1,62 @@
 class MinHeap:
-	array = []
-	count = 0
-	capacity = 0
-	def __init__(self, capacity):
-		self.capacity = capacity
+    def __init__(self, array):
+        self.heap = self.buildHeap(array)
 
-	def parent(self, i):
-		if i <=0 and i >= self.count:
-			return -1
-		else:
-			return (i-1) // 2
+    def buildHeap(self, array):
+        self.heap = array
+        n = len(array)
+        for i in range((n//2)-1, -1, -1):
+            self.siftDown(i)
+        return self.heap
+	
+    def _parent(self, i):
+        if i == 0: return -1
+        return (i-1) // 2
+    
+    def _left(self, i):
+        left_index = i * 2 + 1
+        if left_index >= len(self.heap): return -1
+        return left_index
+	
+    def _right(self, i):
+        right_index = i * 2 + 2
+        if right_index >= len(self.heap): return -1
+        return right_index
+	
+    def _swap(self, i, j):
+        self.heap[i], self.heap[j] = self.heap[j], self.heap[i]
 
-	def leftChild(self, i):
-		left = (2 * i)+1
-		if left > self.count:
-			return -1
-		return left
+    def siftDown(self, i):
+        left_child_index = self._left(i)
+        min_index = i
+        if -1 != left_child_index and self.heap[left_child_index] < self.heap[min_index]:
+            min_index = left_child_index
+        right_child_index = self._right(i)
+        if -1 != right_child_index and self.heap[right_child_index] < self.heap[min_index]:
+            min_index = right_child_index
+        
+        if min_index != i:
+            self._swap(i, min_index)
+            self.siftDown(min_index)
+			
+    def siftUp(self, i):
+        parent_index = self._parent(i)
+        if parent_index != -1:
+            while parent_index != -1 and self.heap[parent_index] > self.heap[i]:
+                self._swap(i, parent_index)
+                i = parent_index
+                parent_index = self._parent(i)
 
-	def rightChild(self, i):
-		right = (2*i) + 2
-		if right>self.count:
-			return -1
-		return right
+    def peek(self):
+        return self.heap[0]
 
-	def getMinimum(self):
-		if self.count == 0:
-			return -1
-		else:
-			return self.array[0]
+    def remove(self):
+        res = self.heap[0]
+        self._swap(0, len(self.heap)-1)
+        self.heap.pop()
+        self.siftDown(0)
+        return res
 
-	def percolateDown(self, i):
-		l = left(i)
-		r  = right(i)
-
-		if -1 != l and self.array[l] < self.array[i]:
-			small = l 
-		else:
-			small = i
-
-		if -1 != r and self.array[r] < self.array[small]:
-			small  = r
-
-		if i != small: 
-			temp = self.array[i]
-			self.array[i] = self.array[small]
-			self.array[small] = temp
-			percolateDown(small)
-
-	def deleteMin(self):
-		if self.count == 0:
-			return -1
-		data = self.array[0]
-		self.array[0] = self.array[self.count-1]
-		self.count -= 1
-		percolateDown(0)
-		return data
-
-	def resizeHeap(self):
-		self.capacity *= 2
-
-	def insert(self, data):
-		if self.count == self.capacity:
-			self.resizeHeap()
-
-		self.count += 1
-		i = self.count - 1
-
-		while i>=0 and data < self.array[parent(i)]:
-			self.array[parent(i)] = self.array[i]
-			i = parent(i)
-
-		self.array[i] = data
-
-	def destroyHeap(self):
-		self.count = 0
-		self.array = []
+    def insert(self, value):
+        self.heap.append(value)
+        self.siftUp(len(self.heap) - 1)
